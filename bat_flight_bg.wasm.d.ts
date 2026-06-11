eff482897c99abe304ceb4b2da19bf99d006a9e0
ABC3Dz <archueyouler@gmail.com>
2026-06-11 16:31:19 +0700

    play on web

---
+++ bat_flight_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_89871: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_89944: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_99358: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_3: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_4: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99358_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_99357: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_94778: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_export5: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
