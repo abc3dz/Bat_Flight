7b0d8a2ac1da9f28f5f8208ede27affd420d7f8e
ABC3Dz <archueyouler@gmail.com>
2026-06-15 20:31:47 +0700

    check coin, gear, heart

--- src/score.rs
+++ src/score.rs
@@ -3,13 +3,26 @@ use bevy::prelude::*;
 use crate::bat_lpl::Bat;
 use crate::coin_lpl::Coin;
 use crate::gear_lpl::Gear;
+use crate::heart_lpl::Heart;
 use crate::GameState;
 use crate::LevelState;
 
-#[derive(Resource, Default)]
+#[derive(Resource)]
 pub struct Score {
     pub value: u32,
+    pub heart: u32,
 }
+impl Default for Score {
+    fn default() -> Self {
+        Self {
+            value: 0,
+            heart: 3,
+        }
+    }
+}
+
+#[derive(Component)]
+struct HeartsContainer;
 
 #[derive(Component)]
 pub struct ScoreText;
@@ -19,7 +32,7 @@ pub struct ScorePlugin;
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
         app
-            .init_resource::<Score>()
+            .insert_resource(Score::default())
             .add_systems(OnEnter(GameState::Playing), setup_score_ui)
             //.add_systems(OnExit(GameState::Playing), cleanup_score_ui)
             .add_systems(Update,check_level_progress.run_if(in_state(GameState::Playing)),)
@@ -29,6 +42,7 @@ impl Plugin for ScorePlugin {
 
 fn setup_score_ui(mut commands: Commands, score: Res<Score>, asset_server: Res<AssetServer>) {
      commands.spawn((
+        HeartsContainer,
         Node {
             position_type: PositionType::Absolute,
             top: px(10.0),
@@ -37,8 +51,9 @@ fn setup_score_ui(mut commands: Commands, score: Res<Score>, asset_server: Res<A
             ..default()
         },
     )).with_children(|parent| {
-        for _ in 0..3 {
+        for _ in 0..score.heart {
             parent.spawn((
+                Heart,
                 ImageNode::new(
                     asset_server.load("images/heart.png")
                 ),
@@ -54,7 +69,7 @@ fn setup_score_ui(mut commands: Commands, score: Res<Score>, asset_server: Res<A
     commands.spawn((
         Node {
             width: Val::Percent(100.0),
-            justify_content: JustifyContent::Center,  // จัดกลางแนวนอน
+            justify_content: JustifyContent::Center,
             padding: UiRect::top(px(10.0)),
             ..default()
         },
@@ -88,9 +103,13 @@ fn check_score(
     bat_query: Query<&Transform, With<Bat>>,
     coin_query: Query<(Entity, &Transform), With<Coin>>,
     gear_query: Query<(Entity, &Transform), With<Gear>>,
+    heart_query: Query<Entity, With<Heart>>,
+    heart_query_trans: Query<(Entity, &Transform), With<Heart>>,
+    container_query: Query<Entity, With<HeartsContainer>>,
     mut score: ResMut<Score>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut next: ResMut<NextState<GameState>>, 
 ) {
     let Ok(bat_t) = bat_query.single() else { return };
 
@@ -100,26 +119,60 @@ fn check_score(
             .distance(coin_transform.translation);
 
         if distance < 1.0 {
-            score.value += 5;
+            if coin_transform.translation.y < -1.0 {
+                score.value += 5;
+            }else{
+                score.value += 1;
+            }
             commands.entity(entity).despawn();
             commands.spawn(AudioPlayer::new(
                 asset_server.load("sounds/score.ogg"),
             ));
-            //println!("Coin collected!");
         }
+    }
 
-    for (entity, coin_transform) in &gear_query {
+    for (entity, gear_transform) in &gear_query {
         let distance = bat_t
             .translation
-            .distance(coin_transform.translation);
+            .distance(gear_transform.translation);
 
         if distance < 1.0 {
-            score.value -= 1;
+            if score.heart <= 0 {
+                score.heart = 3;
+                next.set(GameState::GameOver);
+            }else{
+                score.value -= 1;
+                score.heart -= 1;
+            }
             commands.entity(entity).despawn();
-            // commands.spawn(AudioPlayer::new(
-            //     asset_server.load("sounds/score.ogg"),
-            // ));
+            
+            if let Some(heart_entity) = heart_query.iter().next() {
+                commands.entity(heart_entity).despawn();
+            }
+        }
+    }
 
+    for (entity, heart_trans) in &heart_query_trans {
+        let distance = bat_t
+            .translation
+            .distance(heart_trans.translation);
+
+        if distance < 1.0 {
+            score.heart += 1;
+            commands.entity(entity).despawn();
+
+            if let Ok(container) = container_query.single() {
+                commands.entity(container).with_children(|parent| {
+                    parent.spawn((
+                        Heart,
+                        ImageNode::new(asset_server.load("images/heart.png")),
+                        Node {
+                            width: px(50.0),
+                            height: px(50.0),
+                            ..default()
+                        }
+                    ));
+                });
             }
         }
     }
